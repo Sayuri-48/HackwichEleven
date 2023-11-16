7c869ddbc0bf10abad603c3bd0ecb2b706f969d7
Sayuri-48 <sayuri48@hawaii.edu>
2023-11-16 12:48:02 -1000

    Completed Part 3

--- HackwichEleven/ViewController.swift
+++ HackwichEleven/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func myGuessButtonPressed(_ sender: Any) 
+    {
+        
+    }
+    
+    @IBAction func sliderHasMoved(_ sender: Any) 
+    {
+        
+    }
+    
+    
 }
 
